fa11639bbdb534fc01b99f1be3727c2925c25f5e
Dogukan <dogukangulyasar@gmail.com>
2021-01-29 19:23:54 +0300

    Added all films, added styles

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,16 +1,24 @@
-import React, { Component } from 'react'
-import Row from './Row';
-import requests from './requests';
-
-
+import React, { Component } from "react";
+import Row from "./Row";
+import requests from "./requests";
+import "./App.css";
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Row title="Netfilix Originals" fetchUrl = {requests.fetchNetflixOriginals} />
-        <Row title="Trending Now" fetchUrl = {requests.fetchTrending} />
-        
+        <Row
+          title="NETFILIX ORGIGINALS"
+          fetchUrl={requests.fetchNetflixOriginals}
+          isLargeRow
+        />
+        <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+        <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+        <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+        <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+        <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+        <Row title="Romance Movies " fetchUrl={requests.fetchRomanceMovies} />
+        <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
       </div>
-    )
+    );
   }
 }
