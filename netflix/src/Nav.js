c17a603c8da1912a2b46c9647442a728e3756a68
Dogukan <dogukangulyasar@gmail.com>
2021-02-01 22:08:43 +0300

    Nav features added

--- netflix/src/Nav.js
+++ netflix/src/Nav.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Nav.css";
 
 function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else {
+        handleShow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
   return (
-    <div className="nav">
+    <div className={`nav ${show && "nav_black"}`}>
       <img
         className="nav_logo"
         src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
